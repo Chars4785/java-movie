470710f9e7d5d542bcf75a7dc4fd4584c253acf4
Unknown <9105lgm@naver.com>
2019-04-17 04:45:14 +0900

    금액과 관련된 출력 부분 구현 (시험 아님)

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,6 +6,8 @@ import domain.ScheduleMovie;
 import java.util.List;
 
 public class OutputView {
+    private static final char NEW_LINE = '\n';
+
     public static void printMovies(List<Movie> movies) {
         for (Movie movie : movies) {
             System.out.println(movie);
@@ -20,6 +22,20 @@ public class OutputView {
         for(ScheduleMovie each : books){
             System.out.println(each.toString());
         }
+    }
+
+    public static String printResultCalcue(float discount,int point, int money){
+        int discountedMoney = money - point;
+
+        return String.format("최종 결제한 금액은 "+ checkPoint(discountedMoney,discount)+"원 입니다."+NEW_LINE+
+        "예매를 완료했습니다. 즐거운 영화 관람되세요.");
+    }
+
+    public static float checkPoint(int money, float discount){
+        if(money <= 0){
+            return 0;
+        }
+        return money - ( money * discount);
     }
 
 }
